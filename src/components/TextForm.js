bb4cfbcc4057054cd1432af5fa86f997c8390740
satyaki2001 <satyakisinha0@gmail.com>
2022-04-13 22:56:57 +0530

    update 3

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -19,30 +19,36 @@ export default function TextForm(props) {
     setText(newText);
     props.showAlert("The textbox is cleared", "success");
   }
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showAlert("Extra spaces removed!", "success");
+}
 
     const handleOnChange = (event)=>{
       console.log("On Change!");
       setText(event.target.value);
     }
   
-  const [text, setText] = useState('Enter text here!');
+  const [text, setText] = useState("");
   return (
     <>
     <div className="container" style={{color:props.mode==='dark'?'white':'black'}} >
-      <h1>{props.heading}</h1>
+      <h2 className='mb-4'>{props.heading}</h2>
 <div className="mb-3">
-  <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" style={{backgroundColor: props.mode==='dark'?'grey':'white', color:props.mode==='dark'?'white':'black'}}></textarea>
+  <textarea className="form-control" placeholder="Enter text here!" value={text} onChange={handleOnChange} id="myBox" rows="8" style={{backgroundColor: props.mode==='dark'?'#2D5071':'white', color:props.mode==='dark'?'white':'black'}}></textarea>
 </div>
 <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Upper Case</button>
 <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lower Case</button>
 <button className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+<button className="btn btn-primary ms-1 my-1" onClick={handleExtraSpaces}>Remove extra spaces</button>
     </div>
-    <div className="container my-2" style={{color:props.mode==='dark'?'white':'black'}}>
+    <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
       <h2>Your text summary</h2>
       <p>{text.length>0 ? text.trim().split(" ").length : 0} words and {text.replace(/\s/g, "").length} characters</p>
       <p>Reading time: {0.008 * text.replace(/\s/g, "").length} </p>
       <h2>Preview</h2>
-      <p>{text.length>0?text:"Enter text above to get the preview"}</p>
+      <p>{text.length>0?text:"No text to preview"}</p>
     </div>
     </>
   )
